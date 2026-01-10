944f5a73a5aaf34bff240aa09b43a831a5ac8d7f
Jatin Chaudhary <116703023+jatinchaudhary20@users.noreply.github.com>
2026-01-10 14:12:20 +0530

    final

---
+++ Frontend/src/utils/axiosClient.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const axiosClient =  axios.create({
+    baseURL:'https://localhost:3000',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+
+export default axiosClient;
+
